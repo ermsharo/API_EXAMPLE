d097732b67b2f7f9fb16ca8c831fc14d0415e4f5
Emilio Haro <ermsharo@gmail.com>
2023-11-07 15:50:53 -0300

    Merge branch 'main' of github.com:ermsharo/API_EXAMPL

--- client-api/src/app/page.tsx
+++ client-api/src/app/page.tsx
@@ -1,24 +1,22 @@
-"use client";
-import Header from "@/components/Header";
-import { isLogged } from "@/services/localDataManager";
+'use client';
+import Header from '@/components/Header';
+import { isLogged } from '@/services/localDataManager';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '../theme/theme';
+import Loading from '@/components/Loading';
 export default function Home() {
-  if (isLogged()) {
-    location.assign("http://localhost:3000/recipes");
-  } else {
-    location.assign("http://localhost:3000/login");
-  }
+	if (isLogged()) {
+		location.assign('http://localhost:3000/recipes');
+	} else {
+		location.assign('http://localhost:3000/login');
+	}
 
-
-
-  return (
-    <main>
- 
- <ThemeProvider theme={theme}>
-  <Header />
-
-</ThemeProvider>
-    </main>
-  );
+	return (
+		<main>
+			<ThemeProvider theme={theme}>
+				<Header />
+				<Loading />
+			</ThemeProvider>
+		</main>
+	);
 }
--- client-api/src/components/Loading.tsx
+++ client-api/src/components/Loading.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import styled from "@emotion/styled";
+
+
+const UserDisplay = styled(Box)`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin: auto;
+  padding: 2rem;
+`;
+
 
 export default function Loading() {
   return (
-    <Box sx={{ display: "flex" }}>
+    <UserDisplay sx={{ display: "flex" }}>
       <CircularProgress />
-    </Box>
+    </UserDisplay>
   );
 }
